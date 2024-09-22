772f3173ee46675eee2972ae4a8149100313729a
Girish <girish.ikhe.vu@gmail.com>
2024-09-22 21:45:54 +0530

    All dependencies installed

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="long_term_deposit_prediction",
+    version="0.0.0",
+    author="Girish",
+    author_email="girish.ikhe.@gmail.com",
+    packages=find_packages()
+)
