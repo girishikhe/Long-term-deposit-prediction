f500e360edab0fd7adb0752176c4b32a8fecc53a
Girish <girish.ikhe.vu@gmail.com>
2024-09-24 17:36:58 +0530

    pymongo demo added

--- demo.py
+++ demo.py
@@ -1,2 +1,10 @@
 from long_term_deposit_prediction.logger import logging
+from long_term_deposit_prediction.exception import DepositException
+import sys
+
 logging.info("welcome to custom log")
+try:
+    a=2/0
+except Exception as e:
+    raise DepositException(e,sys)
+
